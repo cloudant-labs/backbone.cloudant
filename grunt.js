61893ed78b757dae33bd21e84b107fd3cdca2826
Simon Metson <simon+github@cloudant.com>
2012-10-30 18:37:55 +0000

    Only remove generated docs on clean, not the css.

--- grunt.js
+++ grunt.js
@@ -29,7 +29,7 @@ module.exports = function(grunt) {
     clean: ["dist/",
             "test/attachments/backbone.cloudant.js",
             "test/attachments/backbone.cloudant.min.js",
-            "docs/"],
+            "docs/*.html"],
     min: {
       dist: {
         src: ['dist/<%= pkg.name %>.js'],
